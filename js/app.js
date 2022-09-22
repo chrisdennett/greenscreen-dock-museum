5c9b7f284060a21d1a996f52524ed0af06437864
chrisdennett <chrisdennett@gmail.com>
2022-09-22 12:24:54 +0100

    Added lots of controls

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@ import { reloadAfterMs } from "./setupWebcam.js";
 
 // app elements
 const artCanvas = document.querySelector("#artCanvas");
-const guideCanvas = document.querySelector("#guideCanvas");
+// const guideCanvas = document.querySelector("#guideCanvas");
 const video = document.querySelector("#videoElement");
 
 const artCtx = artCanvas.getContext("2d");
@@ -11,7 +11,7 @@ const artCtx = artCanvas.getContext("2d");
 let glfxCanvas, texture;
 
 // draw loop
-export function draw({ webcamRes }) {
+export function draw({ webcamRes, params, initParams }) {
   // if (!params) reloadAfterMs();
 
   if (video.srcObject && !video.srcObject.active) {
@@ -21,7 +21,6 @@ export function draw({ webcamRes }) {
   if (!glfxCanvas) {
     // fx loaded in index.html script tag
     glfxCanvas = fx.canvas();
-    console.log("glfxCanvas: ", glfxCanvas);
   }
 
   const frameCanvas = getFlippedVideoCanvas({
@@ -34,7 +33,25 @@ export function draw({ webcamRes }) {
 
   if (glfxCanvas && video) {
     texture = glfxCanvas.texture(frameCanvas);
-    glfxCanvas.draw(texture).ink(0.8).update();
+    let gc = glfxCanvas.draw(texture);
+
+    gc.lensBlur(
+      params.lensBlurRadius,
+      params.lensBlurBrightness,
+      params.lensBlurAngle
+    );
+    gc.triangleBlur(params.triangleBlur);
+    gc.denoise(params.denoise);
+    gc.brightnessContrast(params.brightness, params.contrast);
+    gc.hueSaturation(params.hue, params.saturation);
+    gc.vibrance(params.vibrance);
+    gc.ink(params.ink);
+
+    gc.noise(params.noise);
+    gc.sepia(params.sepia);
+    gc.unsharpMask(params.unsharpRadius, params.unsharpStrength);
+
+    gc.update();
   }
 
   artCanvas.width = webcamRes.w;
@@ -42,3 +59,56 @@ export function draw({ webcamRes }) {
 
   artCtx.drawImage(glfxCanvas, 0, 0);
 }
+
+/*
+ if (currFilter === "ink") 
+ {fxCanvas.draw(texture).ink(inkLevel).update();
+ }
+if (currFilter === "edges") {
+  fxCanvas.draw(texture).edgeWork(edgeLevel).update();
+}
+if (currFilter === "swirl") {
+  fxCanvas
+    .draw(texture)
+    .swirl(380, 240, 200, parseFloat(swirlAngle))
+    .update();
+}
+if (currFilter === "tiltshift") {
+  fxCanvas
+    .draw(texture)
+    .tiltShift(96, 359.25, 480, 287.4, 15, 200)
+    .update();
+}
+if (currFilter === "bulge") {
+  fxCanvas.draw(texture).bulgePinch(320, 239.5, 242, 0.63).update();
+}
+if (currFilter === "pinch") {
+  fxCanvas.draw(texture).bulgePinch(320, 239.5, 242, -0.6).update();
+}
+if (currFilter === "colourhalftone") {
+  fxCanvas.draw(texture).colorHalftone(320, 239.5, 0.87, 5).update();
+}
+if (currFilter === "halftone") {
+  fxCanvas.draw(texture).dotScreen(320, 239.5, 1.1, 4).update();
+}
+if (currFilter === "hexpixels") {
+  fxCanvas.draw(texture).hexagonalPixelate(320, 239.5, 40).update();
+}
+if (currFilter === "unsharp") {
+  fxCanvas.draw(texture).unsharpMask(15, 5).update();
+}
+if (currFilter === "denoise") {
+  fxCanvas.draw(texture).denoise(denoiseLevel).update();
+}
+if (currFilter === "huesat") {
+  fxCanvas.draw(texture).hueSaturation(0, 1).triangleBlur(30).update();
+}
+if (currFilter === "experiment") {
+  fxCanvas
+    .draw(texture)
+    .hueSaturation(0, 0.5)
+    .denoise(denoiseLevel)
+    .ink(inkLevel)
+    .update();
+}
+*/
--- js/index.js
+++ js/index.js
@@ -2,23 +2,30 @@ import { draw } from "./app.js";
 import { setup } from "./setupWebcam.js";
 
 let stats;
-const controls = document.querySelector("#controls");
 
 // const webcamRes = { w: 320, h: 240 };
 const webcamRes = { w: 800, h: 600 };
+
 const params = {
-  cols: 72,
-  rows: 8,
-  gridSize: 0.6000000000000001,
-  xOffset: 0.258,
-  yOffset: 0.247,
-  freq: 0.002,
-  amp: 90,
-  animSpeed: -10,
-  bgColour: "#000000",
-  colourRange: "portland",
-  colourAlpha: 1,
+  brightness: 0,
+  contrast: 0,
+  denoise: 100,
+  hue: 0,
+  saturation: 0,
+  noise: 0,
+  sepia: 0,
+  unsharpRadius: 0,
+  unsharpStrength: 0,
+  vibrance: 0,
+  vibranceStrength: 0,
+  lensBlurRadius: 0,
+  lensBlurBrightness: 1,
+  lensBlurAngle: 0,
+  triangleBlur: 0,
+  edgeWork: 0,
+  ink: 0,
 };
+const initParams = { ...params };
 
 addFps();
 setupControls();
@@ -27,7 +34,7 @@ doDraw();
 
 function doDraw() {
   if (stats) stats.begin();
-  draw({ webcamRes });
+  draw({ webcamRes, params, initParams });
   if (stats) stats.end();
 
   window.requestAnimationFrame(doDraw);
@@ -46,15 +53,102 @@ function setupControls() {
   const gui = new dat.GUI();
   gui.useLocalStorage = true;
   gui.remember(params);
-  // general layout, positions etc
-  const layout = gui.addFolder("layout");
-  layout.add(params, "cols").min(2).max(100).step(1).onChange(updateThing);
-  layout.add(params, "rows").min(2).max(100).step(1).onChange(updateThing);
+  // general filters, positions etc
+  const filters = gui.addFolder("filters");
+  filters.add(params, "brightness").min(-1).max(1).step(0.001);
+  filters.add(params, "contrast").min(-1).max(1).step(0.001);
+  filters.add(params, "denoise").min(0).max(100).step(1);
+  filters.add(params, "hue").min(-1).max(1).step(0.001);
+  filters.add(params, "saturation").min(-1).max(1).step(0.001);
+  filters.add(params, "noise").min(0).max(1).step(0.001);
+  filters.add(params, "sepia").min(0).max(1).step(0.001);
+  filters.add(params, "unsharpRadius").min(0).max(100).step(1);
+  filters.add(params, "unsharpStrength").min(0).max(100).step(1);
+  filters.add(params, "vibrance").min(-1).max(1).step(0.001);
+  filters.add(params, "lensBlurRadius").min(0).max(50).step(1);
+  filters.add(params, "lensBlurBrightness").min(-1).max(1).step(0.001);
+  filters.add(params, "lensBlurAngle").min(0).max(3.1416).step(0.001);
+  filters.add(params, "triangleBlur").min(0).max(200).step(1);
+  filters.add(params, "edgeWork").min(0).max(200).step(1);
+  filters.add(params, "ink").min(0).max(1).step(0.001);
 
   // starting folder state
-  layout.closed = false;
+  filters.closed = false;
 }
 
-function updateThing(value) {
-  console.log("value: ", value);
+/*
+{
+  "preset": "watercolour-2",
+  "closed": false,
+  "folders": {
+    "filters": {
+      "preset": "Default",
+      "closed": false,
+      "folders": {}
+    }
+  },
+  "remembered": {
+    "Default": {
+      "0": {
+        "brightness": 0,
+        "contrast": 0,
+        "denoise": 100,
+        "hue": 0,
+        "saturation": 0,
+        "noise": 0,
+        "sepia": 0,
+        "unsharpRadius": 0,
+        "unsharpStrength": 0,
+        "vibrance": 0,
+        "lensBlurRadius": 0,
+        "lensBlurBrightness": 0,
+        "lensBlurAngle": 0,
+        "triangleBlur": 0,
+        "edgeWork": 0,
+        "ink": 0
+      }
+    },
+    "watercolour-1": {
+      "0": {
+        "brightness": 0.266,
+        "contrast": 0.112,
+        "denoise": 0,
+        "hue": -0.109,
+        "saturation": -0.263,
+        "noise": 0.016,
+        "sepia": 0.225,
+        "unsharpRadius": 23,
+        "unsharpStrength": 15,
+        "vibrance": -1,
+        "lensBlurRadius": 0,
+        "lensBlurBrightness": -0.418,
+        "lensBlurAngle": 0.742,
+        "triangleBlur": 0,
+        "edgeWork": 0,
+        "ink": 0
+      }
+    },
+    "watercolour-2": {
+      "0": {
+        "brightness": 0.31,
+        "contrast": 0.266,
+        "denoise": 36,
+        "hue": 0,
+        "saturation": 0,
+        "noise": 0.17,
+        "sepia": 0.402,
+        "unsharpRadius": 0,
+        "unsharpStrength": 0,
+        "vibrance": 1,
+        "lensBlurRadius": 2,
+        "lensBlurBrightness": -0.418,
+        "lensBlurAngle": 0.742,
+        "triangleBlur": 0,
+        "edgeWork": 0,
+        "ink": 0.622
+      }
+    }
+  }
 }
+
+*/
