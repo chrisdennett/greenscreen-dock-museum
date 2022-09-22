28b66fd5d09e434656fdb46a230532744ec2a8d5
chrisdennett <chrisdennett@gmail.com>
2022-09-22 16:07:10 +0100

    Added greenscreen canvas code

--- js/app.js
+++ js/app.js
@@ -3,8 +3,156 @@ import { reloadAfterMs } from "./setupWebcam.js";
 
 // app elements
 const artCanvas = document.querySelector("#artCanvas");
-// const guideCanvas = document.querySelector("#guideCanvas");
 const video = document.querySelector("#videoElement");
+const greenscreenCanvas = document.querySelector("#guideCanvas");
+const gl = greenscreenCanvas.getContext("webgl", { premultipliedAlpha: false });
+
+// GREEN SCREEN CODE
+const vs = gl.createShader(gl.VERTEX_SHADER);
+gl.shaderSource(
+  vs,
+  "attribute vec2 c; void main(void) { gl_Position=vec4(c, 0.0, 1.0); }"
+);
+gl.compileShader(vs);
+
+const fs = gl.createShader(gl.FRAGMENT_SHADER);
+gl.shaderSource(fs, document.getElementById("fragment-shader").innerText);
+gl.compileShader(fs);
+if (!gl.getShaderParameter(fs, gl.COMPILE_STATUS)) {
+  console.error(gl.getShaderInfoLog(fs));
+}
+
+const prog = gl.createProgram();
+gl.attachShader(prog, vs);
+gl.attachShader(prog, fs);
+gl.linkProgram(prog);
+gl.useProgram(prog);
+
+const vb = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, vb);
+gl.bufferData(
+  gl.ARRAY_BUFFER,
+  new Float32Array([-1, 1, -1, -1, 1, -1, 1, 1]),
+  gl.STATIC_DRAW
+);
+
+const coordLoc = gl.getAttribLocation(prog, "c");
+gl.vertexAttribPointer(coordLoc, 2, gl.FLOAT, false, 0, 0);
+gl.enableVertexAttribArray(coordLoc);
+
+gl.activeTexture(gl.TEXTURE0);
+const tex = gl.createTexture();
+gl.bindTexture(gl.TEXTURE_2D, tex);
+
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+
+const texLoc = gl.getUniformLocation(prog, "tex");
+const texWidthLoc = gl.getUniformLocation(prog, "texWidth");
+const texHeightLoc = gl.getUniformLocation(prog, "texHeight");
+const keyColorLoc = gl.getUniformLocation(prog, "keyColor");
+const similarityLoc = gl.getUniformLocation(prog, "similarity");
+const smoothnessLoc = gl.getUniformLocation(prog, "smoothness");
+const spillLoc = gl.getUniformLocation(prog, "spill");
+
+function startWebcam() {
+  navigator.mediaDevices
+    .getUserMedia({
+      video: {
+        facingMode: "user",
+        width: { ideal: 1280 },
+        height: { ideal: 720 },
+      },
+    })
+    .then((stream) => {
+      video.srcObject = stream;
+      video.play();
+      function processFrame(now, metadata) {
+        greenscreenCanvas.width = metadata.width;
+        greenscreenCanvas.height = metadata.height;
+        gl.viewport(0, 0, metadata.width, metadata.height);
+        gl.texImage2D(
+          gl.TEXTURE_2D,
+          0,
+          gl.RGB,
+          gl.RGB,
+          gl.UNSIGNED_BYTE,
+          video
+        );
+        gl.uniform1i(texLoc, 0);
+        gl.uniform1f(texWidthLoc, metadata.width);
+        gl.uniform1f(texHeightLoc, metadata.height);
+        const m = document
+          .getElementById("keyColor")
+          .value.match(/^#([0-9a-f]{6})$/i)[1];
+        gl.uniform3f(
+          keyColorLoc,
+          parseInt(m.substr(0, 2), 16) / 255,
+          parseInt(m.substr(2, 2), 16) / 255,
+          parseInt(m.substr(4, 2), 16) / 255
+        );
+        gl.uniform1f(
+          similarityLoc,
+          parseFloat(document.getElementById("similarity").value)
+        );
+        gl.uniform1f(
+          smoothnessLoc,
+          parseFloat(document.getElementById("smoothness").value)
+        );
+        gl.uniform1f(
+          spillLoc,
+          parseFloat(document.getElementById("spill").value)
+        );
+        gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+        video.requestVideoFrameCallback(processFrame);
+      }
+      video.requestVideoFrameCallback(processFrame);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+
+// startWebcam();
+
+function drawGreenscreen({ webcamRes, sourceCanvas }) {
+  greenscreenCanvas.width = webcamRes.w;
+  greenscreenCanvas.height = webcamRes.h;
+  gl.viewport(0, 0, webcamRes.w, webcamRes.h);
+  gl.texImage2D(
+    gl.TEXTURE_2D,
+    0,
+    gl.RGB,
+    gl.RGB,
+    gl.UNSIGNED_BYTE,
+    sourceCanvas
+  );
+  gl.uniform1i(texLoc, 0);
+  gl.uniform1f(texWidthLoc, webcamRes.w);
+  gl.uniform1f(texHeightLoc, webcamRes.h);
+  const m = document
+    .getElementById("keyColor")
+    .value.match(/^#([0-9a-f]{6})$/i)[1];
+  gl.uniform3f(
+    keyColorLoc,
+    parseInt(m.substr(0, 2), 16) / 255,
+    parseInt(m.substr(2, 2), 16) / 255,
+    parseInt(m.substr(4, 2), 16) / 255
+  );
+  gl.uniform1f(
+    similarityLoc,
+    parseFloat(document.getElementById("similarity").value)
+  );
+  gl.uniform1f(
+    smoothnessLoc,
+    parseFloat(document.getElementById("smoothness").value)
+  );
+  gl.uniform1f(spillLoc, parseFloat(document.getElementById("spill").value));
+  gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
+}
+
+// END GREEN SCREEN CODE
 
 const artCtx = artCanvas.getContext("2d");
 
@@ -18,11 +166,6 @@ export function draw({ webcamRes, params, initParams }) {
     reloadAfterMs();
   }
 
-  if (!glfxCanvas) {
-    // fx loaded in index.html script tag
-    glfxCanvas = fx.canvas();
-  }
-
   const frameCanvas = getFlippedVideoCanvas({
     video,
     w: webcamRes.w,
@@ -31,8 +174,15 @@ export function draw({ webcamRes, params, initParams }) {
     flipY: false,
   });
 
+  drawGreenscreen({ webcamRes, sourceCanvas: frameCanvas });
+
+  if (!glfxCanvas) {
+    // fx loaded in index.html script tag
+    glfxCanvas = fx.canvas();
+  }
+
   if (glfxCanvas && video) {
-    texture = glfxCanvas.texture(frameCanvas);
+    texture = glfxCanvas.texture(greenscreenCanvas);
     let gc = glfxCanvas.draw(texture);
     gc.sepia(params.sepia);
 
