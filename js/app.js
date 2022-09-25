624229d28a6dc9a4098200b739ad7149b02f570b
chrisdennett <chrisdennett@gmail.com>
2022-09-25 11:04:57 +0100

    REMOVED MEDIA PIPE OPTION (SELFIE SEG CODE)

--- js/app.js
+++ js/app.js
@@ -11,14 +11,8 @@ import { drawGlFxCanvas, setupGlfxCanvas } from "./utils/drawGlFxCanvas.js";
 const videoColorSelector = document.querySelector("#videoHolder");
 const video = document.querySelector("#videoElement");
 
-const selfieCanvas = document.createElement("canvas");
-const selfieCtx = selfieCanvas.getContext("2d");
-
 const greenscreenCanvas = setupGreenScreenShader();
 const glfxCanvas = setupGlfxCanvas();
-
-let cameraSetUp = false;
-const useMediaPipe = false;
 
 // draw loop
 export function draw({ webcamRes, params, img1 }) {
@@ -29,14 +23,8 @@ export function draw({ webcamRes, params, img1 }) {
     return;
   }
 
-  if (useMediaPipe && !cameraSetUp) {
-    cameraSetUp = true;
-    setUpCamera(webcamRes);
-  }
-
   const frameCanvas = getFlippedVideoCanvas({
-    canvas: useMediaPipe ? selfieCanvas : null,
-    video: useMediaPipe ? null : video,
+    video,
     crop: {
       left: params.cropLeft,
       right: params.cropRight,
@@ -56,39 +44,4 @@ export function draw({ webcamRes, params, img1 }) {
   videoColorSelector.style.display = params.showColorDropper
     ? "inherit"
     : "none";
-}
-
-function setUpCamera(webcamRes) {
-  selfieCanvas.width = webcamRes.w;
-  selfieCanvas.height = webcamRes.h;
-
-  function onResults(results) {
-    selfieCtx.save();
-    selfieCtx.clearRect(0, 0, selfieCanvas.width, selfieCanvas.height);
-    selfieCtx.drawImage(results.segmentationMask, 0, 0);
-
-    // Only overwrite missing pixels.
-    selfieCtx.globalCompositeOperation = "source-in";
-    selfieCtx.drawImage(results.image, 0, 0);
-
-    selfieCtx.restore();
-  }
-
-  const selfieSegmentation = new SelfieSegmentation({
-    locateFile: (file) => {
-      return `/libs/mediapipe/selfie_segmentation/${file}`;
-    },
-  });
-
-  selfieSegmentation.setOptions({ modelSelection: 1 });
-  selfieSegmentation.onResults(onResults);
-
-  const camera = new Camera(video, {
-    onFrame: async () => {
-      await selfieSegmentation.send({ image: video });
-    },
-    width: webcamRes.w,
-    height: webcamRes.h,
-  });
-  camera.start();
 }
